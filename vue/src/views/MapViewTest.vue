f7532fc09bfcba2759a2b59c39f71968a4d8fa65
David McIntyre <daveinthe417@gmail.com>
2022-05-29 10:05:15 -0400

    Begin testing google maps import

---
+++ vue/src/views/MapViewTest.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <GoogleMap />
+  </div>
+</template>
+
+<script>
+import GoogleMap from '@/components/Map'
+
+export default {
+    components: {
+        GoogleMap
+    }
+}
+</script>
+
+<style>
+
+</style>
