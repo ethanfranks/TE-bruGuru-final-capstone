d1fe9cebf55831b1835f0fee3e0176ff7d1485ec
Christina Jordan <christinambjordan@gmail.com>
2022-06-02 15:22:10 -0400

    register and login view updates

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,76 +1,101 @@
 <template>
-  <div id="login" class="text-center">
-    <form class="form-signin" @submit.prevent="login">
-      <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
-      <div
-        class="alert alert-danger"
-        role="alert"
-        v-if="invalidCredentials"
-      >Invalid username and password!</div>
-      <div
-        class="alert alert-success"
-        role="alert"
-        v-if="this.$route.query.registration"
-      >Thank you for registering, please sign in.</div>
-      <label for="username" class="sr-only">Username</label>
-      <input
-        type="text"
-        id="username"
-        class="form-control"
-        placeholder="Username"
-        v-model="user.username"
-        required
-        autofocus
-      />
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      <router-link :to="{ name: 'register' }">Need an account?</router-link>
-      <button type="submit">Sign in</button>
-    </form>
+  <div>
+    <header-component></header-component>
+    <div id="login" class="text-center">
+      <img :src="image" alt="Flight of beer" class="logo" />
+      <form class="form-signin" @submit.prevent="login">
+        <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
+        <div class="alert alert-danger" role="alert" v-if="invalidCredentials">
+          Invalid username and password!
+        </div>
+        <div
+          class="alert alert-success"
+          role="alert"
+          v-if="this.$route.query.registration"
+        >
+          Thank you for registering, please sign in.
+        </div>
+        <label for="username" class="sr-only">Username </label>
+        <input
+          type="text"
+          id="username"
+          class="form-control"
+          placeholder="Username"
+          v-model="user.username"
+          required
+          autofocus
+        />
+        <label for="password" class="sr-only"> Password </label>
+        <input
+          type="password"
+          id="password"
+          class="form-control"
+          placeholder="Password"
+          v-model="user.password"
+          required
+        />
+        <br />
+        <button type="submit">Sign in</button>
+        <br />
+        <br />
+        <router-link :to="{ name: 'register' }">Need an account?</router-link>
+      </form>
+      <footer-component></footer-component>
+    </div>
   </div>
 </template>
 
 <script>
 import authService from "../services/AuthService";
+import headerComponent from "../components/Header.vue";
+import footerComponent from "../components/Footer.vue";
+import image from "../assets/secondBeerFlight.png";
 
 export default {
   name: "login",
-  components: {},
+  components: {
+    headerComponent,
+    footerComponent,
+  },
   data() {
     return {
       user: {
         username: "",
-        password: ""
+        password: "",
       },
-      invalidCredentials: false
+      invalidCredentials: false,
+      image: image,
     };
   },
   methods: {
     login() {
       authService
         .login(this.user)
-        .then(response => {
+        .then((response) => {
           if (response.status == 200) {
             this.$store.commit("SET_AUTH_TOKEN", response.data.token);
             this.$store.commit("SET_USER", response.data.user);
             this.$router.push("/");
           }
         })
-        .catch(error => {
+        .catch((error) => {
           const response = error.response;
 
           if (response.status === 401) {
             this.invalidCredentials = true;
           }
         });
-    }
-  }
+    },
+  },
 };
 </script>
+<style scoped>
+.logo {
+  width: 750px;
+  margin: 75px;
+}
+#login {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,84 +1,109 @@
 <template>
-  <div id="register" class="text-center">
-    <form class="form-register" @submit.prevent="register">
-      <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
-      {{user}}
-      <div class="alert alert-danger" role="alert" v-if="registrationErrors">
-        {{ registrationErrorMsg }}
-      </div>
-      <label for="username" class="sr-only">Username</label>
-      <input
-        type="text"
-        id="username"
-        class="form-control"
-        placeholder="Username"
-        v-model="user.username"
-        required
-        autofocus
-      />
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      
-      <input
-        type="password"
-        id="confirmPassword"
-        class="form-control"
-        placeholder="Confirm Password"
-        v-model="user.confirmPassword"
-        required
-      />
-      <label for="checkbox">Do you own or represent a brewery?</label>
-      <input
-      type="checkbox"
-      id="isBrewer"
-      class="form-control"
-      v-on:change="addBrewerRole($event)"
-      />
-      <router-link :to="{ name: 'login' }">Have an account?</router-link>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">
-        Create Account
-      </button>
-    </form>
+  <div>
+    <header-component></header-component>
+    <div id="register" class="text-center">
+      <img :src="image" alt="Fancy flight of beer" class="logoTwo" />
+
+      <form class="form-register" @submit.prevent="register">
+        <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
+        <div class="alert alert-danger" role="alert" v-if="registrationErrors">
+          {{ registrationErrorMsg }}
+        </div>
+        <label for="username" class="sr-only">Username</label>
+        <input
+          type="text"
+          id="username"
+          class="form-control"
+          placeholder="Username"
+          v-model="user.username"
+          required
+          autofocus
+        />
+        <br />
+        <br />
+        <label for="password" class="sr-only">Password</label>
+        <input
+          type="password"
+          id="password"
+          class="form-control"
+          placeholder="Password"
+          v-model="user.password"
+          required
+        />
+
+        <input
+          type="password"
+          id="confirmPassword"
+          class="form-control"
+          placeholder="Confirm Password"
+          v-model="user.confirmPassword"
+          required
+        />
+        <br />
+        <br />
+        <div>
+          <label for="checkbox">Do you own or represent a brewery?</label>
+          <input
+            type="checkbox"
+            id="isBrewer"
+            class="form-control"
+            v-on:change="addBrewerRole($event)"
+          />
+        </div>
+        <br />
+        <br />
+        <button class="btn btn-lg btn-primary btn-block" type="submit">
+          Create Account
+        </button>
+        <br />
+        <br />
+        <router-link :to="{ name: 'login' }"
+          >Already have an account?</router-link
+        >
+      </form>
+      <footer-component></footer-component>
+    </div>
   </div>
 </template>
 
 <script>
-import authService from '../services/AuthService';
+import authService from "../services/AuthService";
+import headerComponent from "../components/Header.vue";
+import footerComponent from "../components/Footer.vue";
+import image from "../assets/favoritebeerflight.png";
 
 export default {
-  name: 'register',
+  name: "register",
+  components: {
+    headerComponent,
+    footerComponent,
+  },
   data() {
     return {
       user: {
-        username: '',
-        password: '',
-        confirmPassword: '',
-        role: 'Beer Lover',
+        username: "",
+        password: "",
+        confirmPassword: "",
+        role: "Beer Lover",
       },
       registrationErrors: false,
-      registrationErrorMsg: 'There were problems registering this user.',
+      registrationErrorMsg: "There were problems registering this user.",
+      image: image,
     };
   },
   methods: {
     register() {
       if (this.user.password != this.user.confirmPassword) {
         this.registrationErrors = true;
-        this.registrationErrorMsg = 'Password & Confirm Password do not match.';
+        this.registrationErrorMsg = "Password & Confirm Password do not match.";
       } else {
         authService
           .register(this.user)
           .then((response) => {
             if (response.status == 201) {
               this.$router.push({
-                path: '/login',
-                query: { registration: 'success' },
+                path: "/login",
+                query: { registration: "success" },
               });
             }
           })
@@ -86,25 +111,37 @@ export default {
             const response = error.response;
             this.registrationErrors = true;
             if (response.status === 400) {
-              this.registrationErrorMsg = 'Bad Request: Validation Errors';
+              this.registrationErrorMsg = "Bad Request: Validation Errors";
             }
           });
       }
     },
     clearErrors() {
       this.registrationErrors = false;
-      this.registrationErrorMsg = 'There were problems registering this user.';
+      this.registrationErrorMsg = "There were problems registering this user.";
     },
     addBrewerRole(evt) {
-      if(evt.target.checked) {
-        this.user.role='Beer Lover,Brewer'
+      if (evt.target.checked) {
+        this.user.role = "Beer Lover,Brewer";
+      } else {
+        this.user.role = "Beer Lover";
       }
-      else {
-        this.user.role='Beer Lover'
-      }
-    }
+    },
   },
 };
 </script>
 
-<style></style>
+<style>
+.logoTwo {
+  width: 750px;
+  margin: 75px;
+}
+#register {
+  display: flex;
+  justify-content: space-around;
+}
+.text-center {
+  display: flex;
+  align-items: center;
+}
+</style>
