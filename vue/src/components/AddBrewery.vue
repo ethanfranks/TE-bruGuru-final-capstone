ea2fb28cf27d174f4416a0996335483e6851c0c4
Christina Jordan <christinambjordan@gmail.com>
2022-06-02 23:13:06 -0400

    updating addBrewery styling

--- vue/src/components/AddBrewery.vue
+++ vue/src/components/AddBrewery.vue
@@ -1,14 +1,12 @@
 <template>
-  <div>
-    
+  <div class="add-brewery-form">
     <h2>Add Brewery</h2>
-    
     <form v-on:submit.prevent="addNewBrewery">
       <div class="form-element">
         <label for="brewer-id">Brewer ID:</label>
         <input id="brewer-id" type="text" v-model="newBrewery.user_id" />
       </div>
-         <div class="form-element">
+      <div class="form-element">
         <label for="brewery-name">Brewery Name:</label>
         <input
           id="brewery-name"
@@ -21,11 +19,15 @@
         <button v-on:click.prevent="resetForm" type="cancel">Cancel</button>
       </div>
     </form>
+    <div>
+      <img :src="image" alt="brewery" class="brewery-image" />
+    </div>
   </div>
 </template>
 
 <script>
 import BreweryService from "../services/BreweryService";
+import image from "../assets/brewery.png";
 
 export default {
   name: "add-brewery",
@@ -35,6 +37,7 @@ export default {
         user_id: null,
         brewery_name: "",
       },
+      image: image,
     };
   },
   methods: {
@@ -54,4 +57,28 @@ export default {
 </script>
 
 <style>
+.add-brewery-form {
+  top: 200px;
+  border-top: 3px solid black;
+  border-bottom: 3px solid black;
+  position: absolute;
+  padding: whitesmoke;
+  padding-top: 2.5%;
+  padding-bottom: 2.5%;
+  background: whitesmoke;
+  background-clip: content-box;
+  box-shadow: inset 0 0 0 15px whitesmoke;
+  text-align: center;
+  z-index: -1;
+}
+.brewery-image {
+  min-height: 100%;
+  min-width: 1024px;
+  width: 100%;
+  height: auto;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -2;
+}
 </style>
--- vue/src/views/AddBrewery.vue
+++ vue/src/views/AddBrewery.vue
@@ -2,15 +2,18 @@
   <div>
     <h1>Add Brewery</h1>
     <add-brewery />
+    <footer-component></footer-component>
   </div>
 </template>
 
 <script>
 import AddBrewery from "../components/AddBrewery.vue";
+import footerComponent from "../components/Footer.vue";
 
 export default {
   components: {
     AddBrewery,
+    footerComponent,
   },
 };
 </script>
